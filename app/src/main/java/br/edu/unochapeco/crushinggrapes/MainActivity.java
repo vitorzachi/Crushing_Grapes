09b99c9d496597725dcb9e8a1d3a0c014ef1371c
Vitor Zachi Junior <vitorzachi@gmail.com>
2015-08-29 14:02:08 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/unochapeco/crushinggrapes/MainActivity.java
@@ -0,0 +1,26 @@
+package br.edu.unochapeco.crushinggrapes;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
