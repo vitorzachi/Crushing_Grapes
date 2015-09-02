cf2dde227ec77989ef30f60c77328d12915102a2
Vitor Zachi Junior <vitorzachi@gmail.com>
2015-09-02 12:57:14 -0300

    Botão no menu e som esmagando

--- app/src/main/java/br/edu/unochapeco/crushinggrapes/MenuScene.java
+++ app/src/main/java/br/edu/unochapeco/crushinggrapes/MenuScene.java
@@ -1,18 +1,39 @@
 package br.edu.unochapeco.crushinggrapes;
 
 import AndGraph.AGGameManager;
+import AndGraph.AGInputManager;
 import AndGraph.AGScene;
+import AndGraph.AGScreenManager;
+import AndGraph.AGSprite;
 
 /**
  * Created by vitor on 29/08/15.
  */
 public class MenuScene extends AGScene {
+
+    private AGSprite botaojogar, botaocredits, botaosair = null;
+
     public MenuScene(AGGameManager pManager) {
         super(pManager);
     }
 
     @Override
     public void init() {
+
+        botaocredits = createSprite(R.drawable.btnsobre, 1, 1);
+        botaocredits.setScreenPercent(90, 20);
+        botaocredits.vrPosition.fX = AGScreenManager.iScreenWidth / 2;
+        botaocredits.vrPosition.fY = AGScreenManager.iScreenHeight / 2;
+
+        botaojogar = createSprite(R.drawable.bntjogar, 1, 1);
+        botaojogar.setScreenPercent(90, 20);
+        botaojogar.vrPosition.fX = AGScreenManager.iScreenWidth / 2;
+        botaojogar.vrPosition.fY = botaocredits.vrPosition.fY + botaojogar.getSpriteHeight();
+
+        botaosair = createSprite(R.drawable.btnsair, 1, 1);
+        botaosair.setScreenPercent(90, 20);
+        botaosair.vrPosition.fX = AGScreenManager.iScreenWidth / 2;
+        botaosair.vrPosition.fY = botaocredits.vrPosition.fY - botaosair.getSpriteHeight();
 
     }
 
@@ -28,6 +49,26 @@ public class MenuScene extends AGScene {
 
     @Override
     public void loop() {
+        if (AGInputManager.vrTouchEvents.backButtonClicked()){
+            vrGameManager.vrActivity.finish();
+        }
+        if (AGInputManager.vrTouchEvents.screenClicked() &&
+                botaojogar.collide(AGInputManager.
+                        vrTouchEvents.getLastPosition())){
+
+            vrGameManager.setCurrentScene(2);
+            return;
+        }
+        if (AGInputManager.vrTouchEvents.screenClicked() &&
+                botaocredits.collide(AGInputManager.
+                        vrTouchEvents.getLastPosition())){
+            vrGameManager.setCurrentScene(4);
+            return;
+        }
+        if (AGInputManager.vrTouchEvents.screenClicked() &&
+                botaosair.collide(AGInputManager.vrTouchEvents.getLastPosition())){
+            vrGameManager.vrActivity.finish();
+        }
 
     }
 }
